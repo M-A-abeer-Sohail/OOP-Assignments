c3f2bccd39f2c98765761ab17a93793a091fadf1
M-A-abeer-Sohail <60892508+M-A-abeer-Sohail@users.noreply.github.com>
2020-03-23 20:10:59 +0500

    Added all files needed

---
+++ game.hpp
@@ -0,0 +1,50 @@
+#pragma once
+#include <SDL.h>
+#include <SDL_image.h>
+#include <stdio.h>
+#include <iostream>
+#include <string>
+#include <stdlib.h>
+#include <time.h>
+#include <list>
+
+#include "pigeon.hpp"
+#include "egg.hpp"
+#include "nest.hpp"
+using namespace std;
+
+class Game{
+    //Screen dimension constants
+    const int SCREEN_WIDTH = 800;
+    const int SCREEN_HEIGHT = 600;
+
+    //The window we'll be rendering to
+    SDL_Window* gWindow = NULL;
+
+    //The window renderer
+    SDL_Renderer* gRenderer = NULL;
+
+    //Current displayed texture
+    SDL_Texture* gTexture = NULL;
+    //global reference to png image sheets
+    SDL_Texture* assets=NULL;
+
+    //You may use C++ linked lists, or you can create your own.
+
+    // list<Pigeon*> pigeons;
+    // list<Egg*> eggs;
+    // list<Nest*> nests;   
+
+
+public:
+
+    bool init();
+    bool loadMedia();
+    void close();
+    SDL_Texture* loadTexture( std::string path );
+    void run();
+    void updateEggs();
+    void updatePigeons();
+    void drawAllObjects();
+};
+
