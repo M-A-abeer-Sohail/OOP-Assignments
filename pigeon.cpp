5dbc3b3687d6dea847695ad7c1f23cc18be49014
M-A-abeer-Sohail <60892508+M-A-abeer-Sohail@users.noreply.github.com>
2020-04-17 09:12:15 +0500

    added .vscode files and trying to draw the objs

--- pigeon.cpp
+++ pigeon.cpp
@@ -1,18 +1,7 @@
 #include "pigeon.hpp"
 
-void Pigeon::draw(SDL_Renderer *testRenderer, bool pause)
-{
-    SDL_RenderCopy(testRenderer, assets, &src[frame], &mover);
-    if (!pause) // If game is paused frame will increment
-    {
-        frame++;
-    }
-    if (frame == 3)
-        frame = 0;
-}
-
 Pigeon::Pigeon(bool empty) : Unit(empty) {}
-Pigeon::Pigeon(SDL_Texture *testTexture)
+Pigeon::Pigeon(SDL_Texture *testTexture, int testThreshold) : threshold(testThreshold)
 {
     src[0] = {0, 0, 160, 133};
     src[1] = {0, 133, 160, 133};
@@ -25,6 +14,28 @@ Pigeon::Pigeon()
     src[1] = {0, 133, 160, 133};
     src[2] = {0, 266, 160, 133};
     assets = NULL;
+}
+
+void Pigeon::draw(SDL_Renderer *testRenderer, bool pause)
+{
+    SDL_RenderCopy(testRenderer, assets, &src[frame], &mover);
+    if (!pause) // If game is paused frame will not increment
+    {
+        frame++;
+    }
+    if (frame == 3)
+        frame = 0;
+}
+void Pigeon::updateBehavior()
+{
+    if (baby)
+    {
+        int *pos = getMover();
+        if (pos[2] == threshold)
+        {
+            baby = false;
+        }
+    }
 }
 
 // void Pigeon::setTexture(SDL_Renderer *tex)
--- pigeon.hpp
+++ pigeon.hpp
@@ -6,16 +6,18 @@ class Pigeon : public Unit
     bool baby;       // is it a baby pigeon?
     SDL_Rect src[3]; // three textures, to make the fly animation
     int frame = 0;
+    int threshold;
 
 public:
     Pigeon();
     Pigeon(bool);
-    Pigeon(SDL_Texture *);
+    Pigeon(SDL_Texture *, int);
     // Pigeon(SDL_Texture *, SDL_Rect);
     // Pigeon(SDL_Texture *, SDL_Rect, bool);
     void draw(SDL_Renderer *, bool);
     bool layEgg();
     bool isAlive();
+    void updateBehavior();
     // bool isAdult();
     //void setTexture(SDL_Renderer *);
 };
